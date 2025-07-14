bd508342b7a53fc7ff2a775727809646789be37c
BlackMugi <tapha10tapha97@gmail.com>
2025-07-14 12:40:27 +0000

    Creation de notre composant food-card

--- Angular-Frontend/src/app/app.component.ts
+++ Angular-Frontend/src/app/app.component.ts
@@ -1,13 +1,16 @@
+
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FoodCardComponent } from './components/food-card/food-card.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, FoodCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular-Frontend';
--- Angular-Frontend/src/app/components/food-card/food-card.component.ts
+++ Angular-Frontend/src/app/components/food-card/food-card.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-food-card',
+  standalone: true,
   imports: [],
   templateUrl: './food-card.component.html',
   styleUrl: './food-card.component.css'
